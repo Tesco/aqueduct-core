9032e2f189e2c1e670659f4dba025f5e23e0641a
Ashish Sharma <ashish.sharma2@tesco.com>
2020-03-19 15:48:14 +0000

    #131 LC/AS/SL: - Added more info around Identity issue token failure

--- pipe-http-server-cloud/src/main/java/com/tesco/aqueduct/pipe/identity/issuer/IdentityIssueTokenProvider.java
+++ pipe-http-server-cloud/src/main/java/com/tesco/aqueduct/pipe/identity/issuer/IdentityIssueTokenProvider.java
@@ -1,10 +1,17 @@
 package com.tesco.aqueduct.pipe.identity.issuer;
 
 import com.tesco.aqueduct.pipe.api.IdentityToken;
+import com.tesco.aqueduct.pipe.api.JsonHelper;
 import com.tesco.aqueduct.pipe.api.TokenProvider;
 import com.tesco.aqueduct.pipe.logger.PipeLogger;
+import io.micronaut.http.HttpHeaders;
+import io.micronaut.http.client.exceptions.HttpClientResponseException;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 public class IdentityIssueTokenProvider implements TokenProvider {
@@ -41,11 +48,36 @@ public class IdentityIssueTokenProvider implements TokenProvider {
                             identityClientSecret
                     )
             );
-        } catch(Exception exception) {
+        } catch (HttpClientResponseException httpException) {
+            try {
+                LOG.info("retrieveIdentityToken",
+                        "request: " + JsonHelper.toJson(new IssueTokenRequest("removedClientId", "removedClientSecret")) +
+                                ", response body: " + httpException.getResponse().body() +
+                                ", response header: " + headers(httpException.getResponse().getHeaders()) +
+                                ", status code: " + httpException.getStatus().getCode()
+                );
+            } catch (IOException e) {
+                LOG.error("retrieveIdentityToken", "Erro while serializing issue token request", e);
+            }
+            throw httpException;
+        }
+        catch(Exception exception) {
+
             LOG.error("retrieveIdentityToken", "trace_id: " + traceId, exception);
             throw exception;
         }
 
         return identityToken;
+    }
+
+    private String headers(HttpHeaders headers) {
+        Iterator<Map.Entry<String, List<String>>> iterator = headers.iterator();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while (iterator.hasNext()) {
+            Map.Entry<String, List<String>> entry = iterator.next();
+            stringBuilder.append(entry.getKey()).append(": ").append(entry.getValue()).append(" ");
+        }
+        return stringBuilder.toString();
     }
 }
