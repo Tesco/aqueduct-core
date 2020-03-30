f432558c312c1a460d1ac98dcb4d1dc4051ea14f
Ashish Sharma <ashish.sharma2@tesco.com>
2020-03-30 15:28:08 +0100

    #131 SL/AS: - Fixed failing PipeCloudServerIntegrationSpec by including clusters table in the DB.

--- pipe-http-server/src/main/java/com/tesco/aqueduct/pipe/http/PipeReadController.java
+++ pipe-http-server/src/main/java/com/tesco/aqueduct/pipe/http/PipeReadController.java
@@ -12,7 +12,6 @@ import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.QueryValue;
 import io.micronaut.security.annotation.Secured;
-import lombok.val;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
