1850eefe38ac1fa78e995d64613cefa935a4209a
hih9 <adam.yorwerth@tesco.com>
2020-08-12 08:35:20 +0100

    - always retry maximum - 1m batch max - 5000 messages max batch - 10 minute retry

--- pipe-storage-postgresql/src/main/java/com/tesco/aqueduct/pipe/storage/PostgresqlStorage.java
+++ pipe-storage-postgresql/src/main/java/com/tesco/aqueduct/pipe/storage/PostgresqlStorage.java
@@ -84,23 +84,7 @@ public class PostgresqlStorage implements CentralStorage {
     }
 
     private long calculateRetryAfter(long queryTimeMs, int messagesCount) {
-        if (messagesCount == 0) {
-            return retryAfter;
-        }
-
-        if (queryTimeMs == 0) {
-            return 1;
-        }
-
-        // retry after = readers / (connections / query time)
-        final double dbThreshold = this.clusterDBPoolSize * 1000 / queryTimeMs;
-        final double retryAfterSecs = this.nodeCount / dbThreshold;
-        final long calculatedRetryAfter = (long) Math.ceil(retryAfterSecs);
-
-        LOG.info("PostgresSqlStorage:calculateRetryAfter:calculatedRetryAfter", String.valueOf(calculatedRetryAfter));
-        LOG.info("PostgresSqlStorage:calculateRetryAfter:retryAfter", String.valueOf(retryAfter));
-
-        return Math.min(calculatedRetryAfter, retryAfter);
+        return retryAfter; // high retry after in all cases
     }
 
     @Override
