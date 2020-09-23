ce623b6d6c06bf49bdc582151fa47608cfc0012d
Ashish Sharma <ashish.sharma2@tesco.com>
2020-09-23 14:17:26 +0100

    AS: * Refactored integration test to be more concise as per review feedback

--- pipe-http-server-cloud/src/integration/groovy/ServerRequestTimeoutIntegrationSpec.groovy
+++ pipe-http-server-cloud/src/integration/groovy/ServerRequestTimeoutIntegrationSpec.groovy
@@ -1,24 +1,17 @@
-import com.opentable.db.postgres.junit.EmbeddedPostgresRules
-import com.opentable.db.postgres.junit.SingleInstancePostgresRule
 import com.stehno.ersatz.Decoders
 import com.stehno.ersatz.ErsatzServer
+import com.tesco.aqueduct.pipe.api.LocationResolver
 import groovy.json.JsonOutput
 import io.micronaut.context.ApplicationContext
 import io.micronaut.inject.qualifiers.Qualifiers
 import io.micronaut.runtime.server.EmbeddedServer
 import io.restassured.RestAssured
 import org.apache.http.NoHttpResponseException
-import org.junit.ClassRule
 import spock.lang.AutoCleanup
 import spock.lang.Shared
 import spock.lang.Specification
 
 import javax.sql.DataSource
-import java.time.ZoneId
-import java.time.ZoneOffset
-import java.time.ZonedDateTime
-
-import static java.util.stream.Collectors.joining
 
 class ServerRequestTimeoutIntegrationSpec extends Specification {
 
@@ -28,30 +21,24 @@ class ServerRequestTimeoutIntegrationSpec extends Specification {
     private static final String CLIENT_ID_AND_SECRET = "trn:tesco:cid:${CLIENT_ID}:${CLIENT_SECRET}"
     public static final String VALIDATE_TOKEN_PATH = "${VALIDATE_TOKEN_BASE_PATH}?client_id=${CLIENT_ID_AND_SECRET}"
 
-    private final static String ISSUE_TOKEN_PATH = "/v4/issue-token/token"
     private final static String ACCESS_TOKEN = UUID.randomUUID().toString()
 
-    private final static String LOCATION_BASE_PATH = "/tescolocation"
-    private final static String LOCATION_CLUSTER_PATH = "/clusters/v1/locations/{locationUuid}/clusters/ids"
-
     @Shared @AutoCleanup ErsatzServer identityMockService
-    @Shared @AutoCleanup ErsatzServer locationMockService
     @Shared @AutoCleanup ApplicationContext context
-    @Shared @ClassRule SingleInstancePostgresRule pg = EmbeddedPostgresRules.singleInstance()
 
-    def setupSpec() {
+    def setup() {
         identityMockService = new ErsatzServer({
             decoder('application/json', Decoders.utf8String)
             reportToConsole()
         })
-
-        locationMockService = new ErsatzServer({
-            decoder('application/json', Decoders.utf8String)
-            reportToConsole()
-        })
-
-        locationMockService.start()
         identityMockService.start()
+
+        def mockLocationResolver = Mock(LocationResolver)
+
+        mockLocationResolver.resolve(_) >> {
+            sleep(3000) // Mocking method processing to be longer than server idle timeout
+            ["someLocation"]
+        }
 
         context = ApplicationContext
             .build()
@@ -67,18 +54,13 @@ class ServerRequestTimeoutIntegrationSpec extends Specification {
                 "authentication.identity.validate.token.path":  "$VALIDATE_TOKEN_PATH",
                 "authentication.identity.client.id":            "$CLIENT_ID",
                 "authentication.identity.client.secret":        "$CLIENT_SECRET",
-                "authentication.identity.issue.token.path":     "$ISSUE_TOKEN_PATH",
                 "authentication.identity.attempts":             "3",
                 "authentication.identity.delay":                "10ms",
                 "authentication.identity.users.userA.clientId": "someClientUserId",
                 "authentication.identity.users.userA.roles":    "PIPE_READ",
 
-                "location.url":                                 "${locationMockService.getHttpUrl() + "$LOCATION_BASE_PATH/"}",
-                "location.attempts":                            3,
-                "location.delay":                               "10ms",
-
                 "compression.threshold-in-bytes":               1024,
-                "micronaut.server.idle-timeout":                "3s",
+                "micronaut.server.idle-timeout":                "1s",
                 // Following config ensures Micronaut retains thread management behaviour from 1.x.x where it chooses
                 // which thread pool (event-loop or IO) to allocate to the controller based on blocking or non-blocking
                 // return type from it.
@@ -86,7 +68,8 @@ class ServerRequestTimeoutIntegrationSpec extends Specification {
             )
             .mainClass(EmbeddedServer)
             .build()
-            .registerSingleton(DataSource, pg.embeddedPostgres.postgresDatabase, Qualifiers.byName("pipe"))
+            .registerSingleton(DataSource, Mock(DataSource), Qualifiers.byName("pipe"))
+            .registerSingleton(LocationResolver, mockLocationResolver)
 
         context.start()
 
@@ -94,45 +77,21 @@ class ServerRequestTimeoutIntegrationSpec extends Specification {
         server.start()
 
         RestAssured.port = server.port
-    }
-
-    void setup() {
         acceptIdentityTokenValidationRequest()
-        issueValidTokenFromIdentity()
-    }
-
-    void cleanup() {
-        identityMockService.clearExpectations()
-        locationMockService.clearExpectations()
-    }
-
-    void cleanupSpec() {
-        RestAssured.port = RestAssured.DEFAULT_PORT
-        context.close()
     }
 
     def "No response from server when it is taking longer than configured server idle timeout"() {
         given: "a location UUID"
         def locationUuid = UUID.randomUUID().toString()
 
-        and: "location service returning clusters for the location uuid with a delay bigger than server idle timeout"
-        locationServiceWithADelayOfMoreThanConfiguredServerIdleTimeout(locationUuid, ["Cluster_A"])
-
         when: "read messages for the given location"
         RestAssured.given()
             .header("Authorization", "Bearer $ACCESS_TOKEN")
             .get("/pipe/0?location=$locationUuid")
 
-        then: "http ok response code"
+        then: "a no http response error is returned"
         thrown(NoHttpResponseException)
     }
-
-
-    private ZonedDateTime utcZoned(String dateTimeFormat) {
-        ZonedDateTime.parse(dateTimeFormat).withZoneSameLocal(ZoneId.of("UTC"))
-    }
-
-    static ZonedDateTime time = ZonedDateTime.now(ZoneOffset.UTC).withZoneSameLocal(ZoneId.of("UTC"))
 
     def acceptIdentityTokenValidationRequest() {
         def json = JsonOutput.toJson([access_token: ACCESS_TOKEN])
@@ -144,7 +103,6 @@ class ServerRequestTimeoutIntegrationSpec extends Specification {
                 called(1)
 
                 responder {
-                    delay(5000)
                     header("Content-Type", "application/json;charset=UTF-8")
                     body("""
                         {
@@ -178,66 +136,4 @@ class ServerRequestTimeoutIntegrationSpec extends Specification {
             }
         }
     }
-
-    private void locationServiceWithADelayOfMoreThanConfiguredServerIdleTimeout(
-            String locationUuid, List<String> clusters) {
-
-        def clusterString = clusters.stream().map{"\"$it\""}.collect(joining(","))
-
-        def revisionId = clusters.isEmpty() ? null : "2"
-
-        locationMockService.expectations {
-            get(LOCATION_BASE_PATH + locationPathIncluding(locationUuid)) {
-                header("Authorization", "Bearer ${ACCESS_TOKEN}")
-                called(1)
-
-                responder {
-                    delay(5000)
-                    header("Content-Type", "application/json")
-                    body("""
-                    {
-                        "clusters": [$clusterString],
-                        "revisionId": "$revisionId"
-                    }
-               """)
-                }
-            }
-        }
-    }
-
-    private String locationPathIncluding(String locationUuid) {
-        LOCATION_CLUSTER_PATH.replace("{locationUuid}", locationUuid)
-    }
-
-    def issueValidTokenFromIdentity() {
-        def requestJson = JsonOutput.toJson([
-                client_id       : CLIENT_ID,
-                client_secret   : CLIENT_SECRET,
-                grant_type      : "client_credentials",
-                scope           : "internal public",
-                confidence_level: 12
-        ])
-
-        identityMockService.expectations {
-            post(ISSUE_TOKEN_PATH) {
-                body(requestJson, "application/json")
-                header("Accept", "application/vnd.tesco.identity.tokenresponse+json")
-                header("Content-Type", "application/json")
-                called(1)
-
-                responder {
-                    header("Content-Type", "application/vnd.tesco.identity.tokenresponse+json")
-                    body("""
-                    {
-                        "access_token": "${ACCESS_TOKEN}",
-                        "token_type"  : "bearer",
-                        "expires_in"  : 1000,
-                        "scope"       : "some: scope: value"
-                    }
-                    """)
-                }
-            }
-        }
-    }
-
 }
