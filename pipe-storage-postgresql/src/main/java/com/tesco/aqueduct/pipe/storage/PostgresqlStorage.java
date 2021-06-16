9338f6b698849e9c4088f786dd1f0c7894f99a9e
sebastianlavender <sebastianlavender@outlook.com>
2021-06-16 15:44:03 +0100

    SL: - turn back on TRANSACTION_REPEATABLE_READ

--- pipe-storage-postgresql/src/main/java/com/tesco/aqueduct/pipe/storage/PostgresqlStorage.java
+++ pipe-storage-postgresql/src/main/java/com/tesco/aqueduct/pipe/storage/PostgresqlStorage.java
@@ -134,7 +134,7 @@ public class PostgresqlStorage implements CentralStorage {
     private Connection getConnectionAndStartTransaction() throws SQLException {
         long start = System.currentTimeMillis();
         Connection connection = pipeDataSource.getConnection();
-//        connection.setTransactionIsolation(Connection.TRANSACTION_REPEATABLE_READ);
+        connection.setTransactionIsolation(Connection.TRANSACTION_REPEATABLE_READ);
         LOG.info("getConnection:time", Long.toString(System.currentTimeMillis() - start));
         return connection;
     }
